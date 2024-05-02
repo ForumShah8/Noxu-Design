5a0592ad215ee96bfa8e67f591e6d25d83232acb
ForumShah8 <shahforu00@gmail.com>
2024-05-02 10:34:51 -0400

    few changes

--- noxu-design/src/app/Components/mainFrame.tsx
+++ noxu-design/src/app/Components/mainFrame.tsx
@@ -4,7 +4,8 @@ import ChatComponent from "./chatInputComponent"
 function HomeFrame(){
     return (
 
-    <div className='grid grid-rows-12 auto-rows-min gap-0 h-screen overflow-y-hidden'>
+    // <div className='grid grid-rows-12 auto-rows-min gap-0 h-screen overflow-y-hidden'>
+    <div className="container h-screen overflow-hidden">
         <InputComponent/>
         <DataTableComponent/>
         <ChatComponent/>
